1cf39053c3002ac2df8e9c8bcfd4a8ce5b693e79
nhutluong <Nhut.Luong@mscoding.org>
2022-03-08 14:23:13 -0600

    Initial commit: Created ASCII_Generator

---
+++ BitmapAscii.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Media;
+
+
+namespace ASCII_Generator {
+    class BitmapAscii {
+        //Fields
+        private int _kernelHeight;
+        private int _kernelWidth;
+        private BitmapMaker _imgPath;
+
+        #region PROPERTIES
+        public int KernelHeight {
+            get { return _kernelHeight; }
+        }//end property
+
+        public int KernelWidth {
+            get { return _kernelWidth; }
+        }//end property
+
+        public BitmapMaker ImgPath {
+            get { return _imgPath; }
+        }//end property
+        #endregion
+
+        #region CONSTRUCTOR
+        public BitmapAscii(BitmapMaker imgPath, int kernelWidth, int kernelHeight) {
+            _kernelWidth = kernelWidth;
+            _kernelHeight = kernelHeight;
+            _imgPath = imgPath;
+        }//end constructor 
+
+        public BitmapAscii() {
+            //parameterless constructor 
+        }//end constructor
+        #endregion
+
+        #region METHOD
+        public BitmapMaker ConvertToGreyScale(string imageFilePath) {
+            BitmapMaker greyBitmap = new BitmapMaker(imageFilePath);
+            double luminosity;//avg rgb value of luminosity
+            Color currentColor;
+
+            //Loop through each pixel to convert orginal rgb to grey value 
+            for (int y = 0; y < greyBitmap.Height; y++) {
+                for (int x = 0; x < greyBitmap.Width; x++) {
+                    //Get pixel color
+                    currentColor = greyBitmap.GetPixelColor(x, y);
+
+                    //Pass color values to get rgb's avg through luminosity method
+                    luminosity = AveragePixel(currentColor);
+
+                    //Set greyscale pixel
+                    greyBitmap.SetPixel(x, y, Color.FromArgb(currentColor.A, (byte)luminosity, (byte)luminosity, (byte)luminosity));
+                }//end for
+            }//end for
+
+            return greyBitmap;
+        }//end method 
+
+        public override string ToString() {
+            string finalAsciiString = Asciitize(ImgPath, KernelWidth, KernelHeight);
+            return finalAsciiString;
+        }//end method 
+
+        private string Asciitize(BitmapMaker imgSource, int kernelWidth, int kernelHeight) {
+            //Contains the ascii text version of the picture
+            string finalAsciiString = String.Empty;
+            double normalizedVal;
+            string asciiCharacter;
+
+            //Convert normalized values to ascii set 
+            for (int y = 0; y < imgSource.Height; y += kernelHeight) {
+                for (int x = 0; x < imgSource.Width; x += kernelWidth) {
+                    //Get current pixel color
+                    Color pixelColor = imgSource.GetPixelColor(x, y);
+
+                    //Get avg pixel value and return normalized value 
+                    normalizedVal = AverageColor(pixelColor);
+
+                    //Convert normalized value to char symbol
+                    asciiCharacter = GreyToString(normalizedVal);
+
+                    //Add symbol to ascii set 
+                    finalAsciiString += asciiCharacter;
+                }//end for
+                //Creates new line when reaching end of width 
+                finalAsciiString += "\n";
+            }//end for
+
+            return finalAsciiString;
+        }//end method 
+
+        private double AveragePixel(Color rgb) {
+            //Luminosity algorithm (the perceived brightness of a colour)
+            //Green is weighted more for perception            
+            return ((.21 * rgb.R) + (.72 * rgb.G) + (.07 * rgb.B));
+        }//end method
+
+        private double AverageColor(Color pixelColors) {
+            double normalizedKernelValue = 0.0;
+            //Returns normalized value from the avg pixel 
+            normalizedKernelValue = (AveragePixel(pixelColors)) / 255;
+
+            return normalizedKernelValue;
+        }//end method       
+
+        private string GreyToString(double normalizedKernelValue) {
+            //Normalized Values to Char Symbols
+            string asciiString = string.Empty;
+
+            if (normalizedKernelValue == 0) {
+                //Fixes the whitespace from turning black 
+                //rgb.A was never changed in averagepixel() so it remained the same in terms of normalized scale 
+                asciiString = " ";
+            } else if (normalizedKernelValue < 0.16) {
+                asciiString = "@";
+            } else if (normalizedKernelValue >= 0.16 && normalizedKernelValue < 0.33) {
+                asciiString = "%";
+            } else if (normalizedKernelValue >= 0.33 && normalizedKernelValue < 0.48) {
+                asciiString = "+";
+            } else if (normalizedKernelValue >= 0.48 && normalizedKernelValue < 0.64) {
+                asciiString = ":";
+            } else if (normalizedKernelValue >= 0.64 && normalizedKernelValue < 0.8) {
+                asciiString = ".";
+            } else if (normalizedKernelValue >= 0.8 && normalizedKernelValue < 1) {
+                asciiString = " ";
+            }//end if
+
+            return asciiString;
+        }//end method
+        #endregion 
+    }//end class
+}//end namespace
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System.Windows;
+using System.Drawing;
+using Microsoft.Win32;
+using System.IO;
+using System.Diagnostics;
+
+namespace ASCII_Generator {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        //Global struct       
+        public string filepath = "";
+        public int kernelWidth = 0;
+        public int kernelHeight = 0;
+        
+
+        public MainWindow() {
+            InitializeComponent();
+        }//end main window
+
+        //BitmapAscii newBitmap = new BitmapAscii();
+        private void MenuItem_OpenClick_1(object sender, RoutedEventArgs e) {
+            
+            //create instance of openfile dialog class
+            OpenFileDialog ofdTemp = new OpenFileDialog();
+
+            //filters only the desired file types 
+            ofdTemp.Filter = "PNG Files | *.png; | JPEG Files | *.jpg; jpeg;";
+
+            //displays dialog box and checks if actual is opened 
+            bool fileSelected = ofdTemp.ShowDialog() == true;
+
+            //keep record of file path 
+            filepath = ofdTemp.FileName;
+
+            //if user selected a file then open the image and send to imagebox
+            if (fileSelected) {//then
+                //create instance of bitmapmaker
+                BitmapMaker originalBitmap = new BitmapMaker(ofdTemp.FileName);
+
+                //set and display source of image box to bitmapmaker
+                imgTestImage.Source = originalBitmap.MakeBitmap();
+
+                
+            }//end if
+
+        }//end event
+        
+        private void MenuItem_SaveClick_1(object sender, RoutedEventArgs e) {
+            //Create instance of openfile dialog class
+            SaveFileDialog saveFile = new SaveFileDialog();
+
+            //Saves file to specific file type 
+            saveFile.Filter = "Text Files|*.txt";
+
+            //Validates if filepath is established
+            if (saveFile.ShowDialog() == true) {
+                //assign to global filepath 
+                filepath = saveFile.FileName;
+
+                //Creates ascii file  
+                StreamWriter writer = new StreamWriter(filepath);
+                writer.Write(txtOutput.Text);
+                writer.Close();
+            }//end if             
+        }//end event
+        private void Button_ClickAsciitize(object sender, RoutedEventArgs e) {
+            if (filepath != string.Empty) {
+                //Convert text input to arguments for kernel  
+                int.TryParse(kWidthTxtBox.Text, out kernelWidth);
+                int.TryParse(kHeightTxtBox.Text, out kernelHeight);
+
+                //Declare bitmap and ascii object
+                BitmapMaker uploadedImage = new BitmapMaker(filepath);
+                BitmapAscii asciiImg = new BitmapAscii(uploadedImage, kernelWidth, kernelHeight);
+
+                //Valid input: Must enter value to begin asciify process
+                if (kernelWidth > 0 && kernelHeight > 0) {
+                    //Initializes ascii processing and outputs ascii to gui 
+                    txtOutput.Text = asciiImg.ToString();
+                } else {
+                    MessageBox.Show("Enter integral values (greater than 0) for kernel width and height");
+                }//end else 
+            } else {
+                MessageBox.Show("Must upload image to asciitize");
+            }//end else 
+        }//end event 
+
+        
+        private void about_Click(object sender, RoutedEventArgs e) {
+            //Retrieve documentation on ascii art 
+
+            //Declare Process object - enables start or stop task processing 
+            Process webDoc = new Process();
+
+            //Specifies os shell should be used to start process
+            webDoc.StartInfo.UseShellExecute = true;
+
+            //Reference file path 
+            webDoc.StartInfo.FileName = "https://en.wikipedia.org/wiki/ASCII_art";
+
+            //Run task 
+            webDoc.Start();
+        }//end event 
+
+        private void MenuItem_ExitClick(object sender, RoutedEventArgs e) {
+            this.Close();
+        }//end event
+
+        private void Button_ClickGreyTheImage(object sender, RoutedEventArgs e) {
+            if (filepath != string.Empty) {
+                //new instance of bitmap          
+                BitmapMaker greyBitmap = new BitmapMaker(filepath);
+
+                //new instance of bitmap ascii
+                BitmapAscii tempBitmap = new BitmapAscii();
+
+                //set greybitmap to our bitmapascii convert to grey scale method
+                greyBitmap = tempBitmap.ConvertToGreyScale(filepath);
+
+                //set greyimage.source to makebitmap method 
+                greyImage.Source = greyBitmap.MakeBitmap();
+            } else {
+                MessageBox.Show("Must upload image to convert to greyscale");
+            }//end else
+        }//end event
+
+    }//end class
+}//end namespace
